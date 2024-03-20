f1d45677c11e0da43cd40081f1b964cf7617199f
ThreeSwords Leaf <921488837@qq.com>
2024-03-20 20:15:16 +0800

    更新了串口总线舵机代码

--- Control_Unit/Software/BSP/Serial_Bus_Servo/Instruction_Set.c
+++ Control_Unit/Software/BSP/Serial_Bus_Servo/Instruction_Set.c
@@ -1,3 +1,12 @@
+/*
+ * @Author: 小叶同学
+ * @Date: 2024-03-20 10:16:35
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2024-03-20 12:39:09
+ * @Description: 请填写简介
+ */
+// Control_Unit\Software\BSP\Serial_Bus_Servo
+
 char *commands[] = {
     "#000P1500T1000!", // 1. 控制舵机
     "#000PVER!",       // 2. 读取舵机版本号
@@ -22,3 +31,45 @@ char *commands[] = {
     "#000PCLE!",       // 21. 全恢复出厂设置
     "#000PRTV!"        // 22. 获取温度和电压
 };
+
+// while (1)
+// {
+
+//     /* USER CODE END WHILE */
+
+//     /* USER CODE BEGIN 3 */
+//     static uint8_t test = 6;
+//     // 使能发送功能，每次发送前需要调用此函数
+//     HAL_HalfDuplex_EnableTransmitter(&huart1);
+//     HAL_UART_Transmit(&huart1, &test, 1, 2000);
+//     // 使能接收功能。每次接收前需要调用此函数
+//     HAL_HalfDuplex_EnableReceiver(&huart1);
+//     HAL_UART_Receive(&huart1, &res, 1, 2000);
+
+//     if (res == 6)
+//     {
+//         test++;
+//     }
+// }
+
+// 主函数调用示例
+#include "servo.h"
+
+UART_HandleTypeDef huart1;
+uint8_t buffer[100];
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
+
+    while (1)
+    {
+        servo_command(&huart1, "#000P1500T1000!"); // 控制舵机
+        HAL_Delay(1000);
+        receive_data(&huart1, buffer); // 接收并处理数据
+        HAL_Delay(1000);
+    }
+}
