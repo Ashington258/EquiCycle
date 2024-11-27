ec6eef77df6241e729b24bfbec88da139fa4e195
Ashington <ashington258@proton.me>
2024-11-28 03:06:31 +0800

    feat(config): 更新输入视频路径并添加车速设置
    - 更新 INPUT_SOURCE 为 "dataset/video/1280.mp4"
    - 添加 CAR_SPEED 设置为 1，用于控制后轮电机速度和车辆行驶速度

--- Calculation_Unit/src/config.py
+++ Calculation_Unit/src/config.py
@@ -4,7 +4,7 @@ class Config:
     MODEL_PATH = "Calculation_Unit/model/lane.pt"
     LANE_MODEL = "Calculation_Unit/model/lane.pt"
     ELEMENTS_MODEL = "Calculation_Unit/model/elements.pt"
-    INPUT_SOURCE = "video/1280.5.mp4"  # 支持图片路径、视频路径、摄像头ID或URL
+    INPUT_SOURCE = "dataset/video/1280.mp4"  # 支持图片路径、视频路径、摄像头ID或URL
     CONF_THRESH = 0.65  # 置信度阈值
     IMG_SIZE = 640  # 输入图像宽度，保持宽高比调整
 
@@ -12,6 +12,7 @@ class Config:
     TARGET_X = 320  # 目标的 X 坐标（可以根据实际需求调整）
     R = 250  # 调节舵机力度的参数，越大舵机力度越小
     SERVO_MIDPOINT = 960  # 舵机中值脉冲宽度
+    CAR_SPEED = 1  # 后轮电机速度，车辆行驶速度
 
     # 定义类别名称
     LANE_CLASS_NAME = [
